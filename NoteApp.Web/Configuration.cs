8ec0020df74370c4e9d89659770e88f91454d9c6
Lucas Miranda <lucas_miranda_chagas@hotmail.com>
2024-11-11 18:05:37 -0300

    feat: added support for light theme and dark theme

---
+++ NoteApp.Web/Configuration.cs
@@ -0,0 +1,39 @@
+using MudBlazor;
+using MudBlazor.Utilities;
+
+namespace NoteApp.Web;
+
+public static class Configuration
+{
+    public static readonly MudTheme Theme = new()
+    {
+        Typography = new Typography()
+        {
+            Default = new Default()
+            {
+                FontFamily = ["Ubuntu", "sans-serif"],
+            }
+        },
+        PaletteLight = new PaletteLight()
+        {
+            Primary = new MudColor("#3B0A77"),
+            Secondary = new MudColor("#e0e0e0"),
+            Tertiary = new MudColor("#6310C0"),
+            AppbarBackground = new MudColor("#e0e0e0"),
+            Background = new MudColor("#3B0A77"),
+            TextPrimary = new MudColor("#e0e0e0"),
+            TextSecondary = new MudColor("#3B0A77"),
+        },
+        PaletteDark = new PaletteDark()
+        {
+            Primary = new MudColor("#e0e0e0"),
+            Secondary = new MudColor("#3B0A77"),
+            AppbarBackground = new MudColor("#3B0A77"),
+            Background = new MudColor("#e0e0e0"),
+            TextPrimary = new MudColor(Colors.Shades.Black),
+            TextSecondary = new MudColor("#e0e0e0"),
+            PrimaryContrastText = new MudColor("#3B0A77"),
+            Surface = new MudColor("#3B0A77"),
+        }
+    };
+}
